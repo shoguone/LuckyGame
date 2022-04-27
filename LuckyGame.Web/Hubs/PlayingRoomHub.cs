4fb0469ade6a6eccea141d5d94f271c25e4fcc09
Vladimir Alemasov <avlal.nsu@gmail.com>
2022-04-28 00:38:34 +0700

    added a hub with simple requests

---
+++ LuckyGame.Web/Hubs/PlayingRoomHub.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace LuckyGame.Web.Hubs;
+
+public class PlayingRoomHub : Hub
+{
+    public const string MethodWait = "wait";
+
+    private const string CurrentRoom = "TheRoom";
+
+    public async Task Connect(string playerName)
+    {
+        await SendWait();
+    }
+    
+    private async Task SendWait()
+    {
+        await Groups.AddToGroupAsync(Context.ConnectionId, CurrentRoom);
+        await Clients.Caller.SendAsync(MethodWait);
+    }
+
+}
